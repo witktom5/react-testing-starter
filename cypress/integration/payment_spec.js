72e07b28bd5ee4f8cecbf53dd7843c8f627bfac8
witktom5 <witktom5@gmail.com>
2022-05-09 18:11:33 +0200

    add cypress

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,14 @@
+describe("payment", () => {
+  it("user can make payment", () => {
+    // login
+    // check account balance
+    // click on pay button
+    // search for user
+    // add amount and note and click pay
+    // return to transactions
+    // go to personal payments
+    // click on payment
+    // verify if payment was made
+    // verify if payment amount was deducted
+  });
+});
