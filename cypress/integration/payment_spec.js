aa5e532d44b52a52748ba0e72d69328a4f660922
witktom5 <witktom5@gmail.com>
2022-05-11 09:54:59 +0200

    add cypress integration test for payment

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -1,14 +1,64 @@
+const { v4: uuidv4 } = require("uuid");
+
 describe("payment", () => {
   it("user can make payment", () => {
-    // login
-    // check account balance
-    // click on pay button
+    //  login
+    cy.visit("/");
+    cy.findByRole("textbox", {
+      name: /username/i,
+    }).type("johndoe");
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", {
+      name: /remember me/i,
+    }).check();
+    cy.findByRole("button", {
+      name: /sign in/i,
+    }).click();
+
+    //  check account balance
+    let oldBalance = cy
+      .get('[data-test="sidenav-user-balance"]')
+      .then(($balance) => (oldBalance = $balance.text()));
+
+    // click on new button
+    cy.findByRole("button", {
+      name: /new/i,
+    }).click();
+
     // search for user
+    cy.findByRole("textbox").type("devon becker");
+    cy.findByText(/devon becker/i).click();
+
     // add amount and note and click pay
+
+    const note = uuidv4();
+    const amount = "20.00";
+    cy.findByPlaceholderText(/amount/i).type(amount);
+    cy.findByPlaceholderText(/add a note/i).type(note);
+    cy.findByRole("button", {
+      name: /pay/i,
+    }).click();
+
     // return to transactions
+
+    cy.findByText(/return to transactions/i).click();
     // go to personal payments
+
+    cy.findByText(/mine/i).click();
     // click on payment
+
+    cy.findByText(note).click({ force: true });
+
     // verify if payment was made
+    cy.findByText(`-$${amount}`).should("be.visible");
+    cy.findByText(note).should("be.visible");
+
     // verify if payment amount was deducted
+
+    cy.get('[data-test="sidenav-user-balance"]').then(($balance) => {
+      const convertedOldBalance = parseFloat(oldBalance.replace(/\$|,/g, ""));
+      const convertedNewBalance = parseFloat($balance.text().replace(/\$|,/g, ""));
+      expect(convertedOldBalance - convertedNewBalance).to.equal(parseFloat(amount));
+    });
   });
 });
